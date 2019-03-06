de835e84a40e9b62e74a7e668cc3be967b1dfb66
MatsukiJinen <jinen850@gmail.com>
2019-03-06 13:41:50 +0900

    Closure

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+)
+
+func later() func(string) string {
+	//１つ前に与えられた文字列を保存するための変数
+	var store string
+	//引数に文字列をとり文字列を返す関数を返す
+	return func(next string) string {
+		s := store
+		store = next
+		return s
+	}
+}
+
+func main() {
+	fmt.Println("Hello World!");
+
+	f := later()
+
+	fmt.Println(f("Golang"))
+	fmt.Println(f("is"))
+	fmt.Println(f("awesome!"))
+}
