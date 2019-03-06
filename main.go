cbaea47115fa6501e431081689103ca06cd298c5
MatsukiJinen <jinen850@gmail.com>
2019-03-06 16:35:26 +0900

    Generator

--- main.go
+++ main.go
@@ -4,14 +4,32 @@ import (
     "fmt"
 )
 
+//関数を返す関数
+
 func later() func(string) string {
 	//１つ前に与えられた文字列を保存するための変数
 	var store string
 	//引数に文字列をとり文字列を返す関数を返す
 	return func(next string) string {
+		// var s string
+ 	// 	s = store
 		s := store
 		store = next
 		return s
+	}
+}
+
+func returnFunc() func() string{
+	return func() string{
+		return "Hello Golang"
+	}
+}
+
+func intergers() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
 	}
 }
 
@@ -19,8 +37,25 @@ func main() {
 	fmt.Println("Hello World!");
 
 	f := later()
+	ff := returnFunc()
+	fff := intergers()
+	ffff := intergers()
 
-	fmt.Println(f("Golang"))
-	fmt.Println(f("is"))
-	fmt.Println(f("awesome!"))
+	ints := intergers()
+
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(fff())
+	fmt.Println(fff())
+	fmt.Println(fff())
+	fmt.Println(fff())
+	fmt.Println(ffff())
+    
+    ff()
+	fmt.Println(f("Golang")) //""
+	fmt.Println(f("is")) // "Golang"
+	fmt.Println(f("awesome!")) // "is"
+
+
 }
